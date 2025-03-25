41a8fe06e5556e86dacdc7bcc887f2686dabbdf3
ShanmukhShashankh <vemula.shanmukhshashankh@gmail.com>
2025-03-25 17:53:36 +0530

    Image.ppm

---
+++ build/CMakeFiles/RayTracinginOneWeekend.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RayTracinginOneWeekend.dir/link.d"
+  "CMakeFiles/RayTracinginOneWeekend.dir/main.cc.o"
+  "CMakeFiles/RayTracinginOneWeekend.dir/main.cc.o.d"
+  "RayTracinginOneWeekend"
+  "RayTracinginOneWeekend.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RayTracinginOneWeekend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
