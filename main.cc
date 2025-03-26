b5fca29e5795a040bfe549f8bf32ee51698aaa2f
ShanmukhShashankh <vemula.shanmukhshashankh@gmail.com>
2025-03-26 23:10:07 +0530

    Red Sphere

--- main.cc
+++ main.cc
@@ -3,10 +3,22 @@
 #include "ray.h"
 #include "vec3.h"
 
+bool hit_sphere(const point3& center, double radius, const ray& r){
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius*radius;
+    auto D = b*b - 4*a*c;
+    return (D >= 0);
+}
+
 color ray_color(const ray& r){
+    if(hit_sphere(point3(0,0,-1), 0.5, r))
+        return color(1,0,0);
+    
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(1.0, 0.7, 0);
 }
 
 int main() {
@@ -42,7 +54,7 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        std::clog << "\nScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
             auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
             auto ray_direction = pixel_center - camera_center;
